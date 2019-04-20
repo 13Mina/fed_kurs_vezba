820170d7d3e1efb2cb164c4b477f38c983e3d175
Mina Pavlica <mina.pavlica13@gmail.com>
2019-04-20 13:39:15 +0200

    zavrsen i js

---
+++ java.js
@@ -0,0 +1,11 @@
+function provera(forma){
+    if(forma.ime.value==""){
+        alert("morate uneti ime");
+        return false;
+    }
+    if(forma.email.value=="" ){
+        alert("morate uneti imejl");
+        return false;
+    }
+
+}
